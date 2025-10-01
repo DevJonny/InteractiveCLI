a2a8dcf5b316860c5343dd065b48b2d63b4bb055
Bogdan Stoica <bogdan.stoica@wearefreemarket.com>
2025-10-01 10:54:52 +0100

    Add generic factory registration for services

---
+++ src/InteractiveCLI/ServiceConfigurationHelpers/DefaultServiceBuilder.cs
@@ -0,0 +1,7 @@
+﻿namespace InteractiveCLI.ServiceConfigurationHelpers;
+
+public class DefaultServiceBuilder<TService>(IServiceCollection services, string name) : IServiceBuilder<TService> where TService : class
+{
+    public string Name { get; } = name;
+    public IServiceCollection Services { get; } = services;
+}
---
+++ src/InteractiveCLI/ServiceConfigurationHelpers/IServiceBuilder.cs
@@ -0,0 +1,7 @@
+﻿namespace InteractiveCLI.ServiceConfigurationHelpers;
+
+public interface IServiceBuilder<TService> where TService : class
+{
+    public string Name { get; }
+    public IServiceCollection Services { get; }
+}
