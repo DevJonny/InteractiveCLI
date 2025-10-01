ce503a4cde9ea4c320f9589e13c5f5d1bc69d46c
Bogdan Stoica <bogdan.stoica@wearefreemarket.com>
2025-10-01 10:54:52 +0100

    Add generic factory registration for services

---
+++ src/InteractiveCLI/ServiceConfigurationHelpers/ServiceFactoryOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace InteractiveCLI.ServiceConfigurationHelpers;
+
+public class ServiceFactoryOptions<TService> where TService : class
+{
+    public List<Action<TService, IServiceProvider>> ServiceActions { get; set; } = new();
+    public List<Func<IServiceProvider, object[]>> ConstructorArgumentProviders { get; set; } = new();
+    public Func<IServiceProvider, object[], TService>? CustomFactory { get; set; }
+}
